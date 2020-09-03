43cefbc05c33450bc6214fde71a7153372e8f6b2
Radioactive92177 <Raju.11811924@lpu.in>
2020-09-03 19:51:06 +0530

    Changed authentication type

--- assets/lib/js/API/HttpLibrary.js
+++ assets/lib/js/API/HttpLibrary.js
@@ -1,26 +1,33 @@
 class DossierLibrary {
   constructor() {
-    this.client_id = "0098a006f7c78d33c9a1";
-    this.client_secret = "9182ccf98c535df0608ab67f84f4ef4bc6cc7de8";
     this.repos_count = 10;
     this.repos_sort = "created: asc";
   }
 
   // Get data from the API
   async getData(user) {
-    const profileResponse = await fetch(
-      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
-    );
-    const repoResponse = await fetch(
-      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
-    );
+    // Getting user data
+    const userResponse = await fetch(`https://api.github.com/users/${user}`, {
+      headers: {
+        Authorization: "token a4169bf97d75a8bdc1188d65355932b55bc9e101",
+      },
+    });
+    const userData = await userResponse.json();
 
-    const profile = await profileResponse.json();
-    const repos = await repoResponse.json();
+    // Getting user repo data
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`,
+      {
+        headers: {
+          Authorization: "token a4169bf97d75a8bdc1188d65355932b55bc9e101",
+        },
+      }
+    );
+    const repoData = await repoResponse.json();
 
     return {
-      profile,
-      repos,
+      userData,
+      repoData,
     };
   }
 }
