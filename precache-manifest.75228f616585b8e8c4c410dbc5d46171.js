b77b524783f345944af8d41a0ce7b53235ce71ef
MG Raju <raju.11811924@lpu.in>
2020-10-25 19:41:24 +0530

    Updates

---
+++ precache-manifest.75228f616585b8e8c4c410dbc5d46171.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a4bdf008c0b0b8a427c96095b0ec328",
+    "url": "/GitHub-Dossier/index.html"
+  },
+  {
+    "revision": "7160ccf1e11d991dfe6c",
+    "url": "/GitHub-Dossier/static/css/main.ddaa0c25.chunk.css"
+  },
+  {
+    "revision": "b77905b4a8e1310391c6",
+    "url": "/GitHub-Dossier/static/js/2.0814bfec.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/GitHub-Dossier/static/js/2.0814bfec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7160ccf1e11d991dfe6c",
+    "url": "/GitHub-Dossier/static/js/main.617dc5d3.chunk.js"
+  },
+  {
+    "revision": "716d91101fa147e4c6c9",
+    "url": "/GitHub-Dossier/static/js/runtime-main.71548a1b.js"
+  }
+]);
--- src/api/github.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.github.com",
-  headers: {
-    authorization: "token 300bceeb7c2fabdf0567d1e0e0f6068526655b55",
-  },
-});
