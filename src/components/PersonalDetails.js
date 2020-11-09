b710fe3be5a907ece433d757ff8baaa23a0fce48
Raj <raju.11811924@lpu.in>
2020-11-10 01:40:25 +0530

    Profile details started showing up

--- src/components/PersonalDetails.js
+++ src/components/PersonalDetails.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
 
-const PersonalDetails = () => {
-    return (
-        <>
-         <h1>Personal Details</h1>   
-        </>
-    )
-}
+const PersonalDetails = ({
+  html_url,
+  public_repos,
+  public_gists,
+  followers,
+  following,
+  name,
+  blog,
+  location,
+  created_at,
+  hireable,
+  twitter_username,
+}) => {
+  return (
+    <>
+      <h1>{name}</h1>
+      <a href={`${html_url}`}>
+        <h1>See Profile</h1>
+      </a>
+      <h4>{public_repos}</h4>
+      <h4>{public_gists}</h4>
+      <h4>{followers}</h4>
+      <h4>{following}</h4>
+      <a href={`${blog}`}>
+        <h3>{blog}</h3>
+      </a>
+      <h4>{location}</h4>
+      <h4>{created_at}</h4>
+      <h4>{hireable}</h4>
+      <h4>{twitter_username}</h4>
+    </>
+  );
+};
 
-export default PersonalDetails
+export default PersonalDetails;
--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
 import ProfilePic from "../components/ProfilePic";
+import PersonalDetails from "../components/PersonalDetails";
 
 export class UserProfile extends Component {
   render() {
+    const {
+      avatar_url,
+      html_url,
+      public_repos,
+      public_gists,
+      followers,
+      following,
+      name,
+      blog,
+      location,
+      created_at,
+      hireable,
+      twitter_username,
+    } = this.props;
     return (
       <>
         <h1>Profile Data</h1>
-        <ProfilePic pic={this.props.avatar_url} />
+        <ProfilePic pic={avatar_url} />
+        <PersonalDetails
+          html_url={html_url}
+          public_repos={public_repos}
+          public_gists={public_gists}
+          followers={followers}
+          following={following}
+          name={name}
+          blog={blog}
+          location={location}
+          created_at={created_at}
+          hireable={hireable}
+          twitter_username={twitter_username}
+        />
       </>
     );
   }
