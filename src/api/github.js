3fe70366f8e64d1f953f2906723ff185f0a64b4b
Raj <raju.11811924@lpu.in>
2020-11-18 14:56:30 +0530

    Update github.js

--- src/api/github.js
+++ src/api/github.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.github.com",
   headers: {
-    authorization: "token 300bceeb7c2fabdf0567d1e0e0f6068526655b55",
+    authorization: "token a12dc2865ac6f95b3cd2d48ad4900bca7afb310e",
   },
 });
