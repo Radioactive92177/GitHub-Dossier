f6679f8abc8b42d0d47b22af59629941a37ae7a6
Raj <raju.11811924@lpu.in>
2024-05-25 12:50:23 +0530

    Update github.js
    Testing without Authorization

--- src/api/github.js
+++ src/api/github.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.github.com",
-  headers: {
-    authorization: "token a12dc2865ac6f95b3cd2d48ad4900bca7afb310e",
-  },
+  baseURL: "https://api.github.com"
 });
